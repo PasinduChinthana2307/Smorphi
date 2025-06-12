99f7f0028298302e2fc5e0c09f43a4e479c3240e
Pasindu Chinthana <pasinduchinthana678@gmail.com>
2025-06-12 18:25:18 +0530

    Update mpu9250driver_launch.py

--- ros2_mpu9250_driver-main/launch/mpu9250driver_launch.py
+++ ros2_mpu9250_driver-main/launch/mpu9250driver_launch.py
@@ -3,20 +3,18 @@ from launch import LaunchDescription
 from launch_ros.actions import Node
 from launch.actions import DeclareLaunchArgument
 from launch.substitutions import LaunchConfiguration
-
 import os
-
 
 def generate_launch_description():
     ld = LaunchDescription()
     share_dir = get_package_share_directory('mpu9250driver')
     parameter_file = LaunchConfiguration('params_file')
-
+    
     params_declare = DeclareLaunchArgument('params_file',
                                            default_value=os.path.join(
                                                share_dir, 'params', 'mpu9250.yaml'),
                                            description='Path to the ROS2 parameters file to use.')
-
+    
     mpu9250driver_node = Node(
         package='mpu9250driver',
         executable='mpu9250driver',
@@ -25,7 +23,15 @@ def generate_launch_description():
         emulate_tty=True,
         parameters=[parameter_file]
     )
-
+    
+    static_tf = Node(
+        package="tf2_ros",
+        executable="static_transform_publisher",
+        arguments=["0", "0", "0", "0", "0", "0", "base_link", "imu_link"]
+    )
+    
     ld.add_action(params_declare)
     ld.add_action(mpu9250driver_node)
+    ld.add_action(static_tf)
+    
     return ld
